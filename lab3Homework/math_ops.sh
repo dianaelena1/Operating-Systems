2d2796e8cea86f9c7ebab2fd0f1f4833f2948313
Paul Pogacean <pogaceanpaul@gmail.com>
2026-03-06 13:40:41 +0000

    Homework 1 - Pogacean Paul

--- lab3Homework/math_ops.sh
+++ lab3Homework/math_ops.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# In bash scripts, $1 is the first parameter passed, and $2 is the second.
+# $1- first parameter passed, $2 - the second.
 
-# Calculate the results using $(( ... )) for math operations
+# use $(( ... )) for math operations
 sum=$(($1 + $2))
 difference=$(($1 - $2))
 product=$(($1 * $2))
 
-# Display the results
 echo "The sum is: $sum"
 echo "The difference is: $difference"
 echo "The product is: $product"
