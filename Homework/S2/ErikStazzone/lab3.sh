b32bb4a64bd73c0f1b7c9a791d060bea2d2033c7
stazoone <erikstazzone04@gmail.com>
2026-03-20 10:15:22 +0200

    added homework Stazzone Erik 822/2

---
+++ Homework/S2/ErikStazzone/lab3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+input_file="who_output.txt"
+
+echo "full name|IP address"
+
+while read -r line; do
+    user=$(echo "$line" | awk '{print $1}')
+
+    ip=$(echo "$line" | grep -o '([^)]*)' | sed 's/[()]//g')
+
+    if [ -z "$ip" ]; then
+        ip="Local"
+    fi
+
+    fullname=$(getent passwd "$user" | cut -d: -f5 | cut -d, -f1)
+
+
+    [ -z "$fullname" ] && fullname="$user"
+
+    printf "%-20s %-20s\n" "$fullname" "$ip"
+done < "$input_file"
