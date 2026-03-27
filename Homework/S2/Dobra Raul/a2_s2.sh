69e4a16fb8002f59c709ff3aca942b62b1829dd1
DobraRaul <raul.dobra@stud.ubbcluj.ro>
2026-03-27 11:19:19 +0200

    a2: Dobra Raul

---
+++ Homework/S2/Dobra Raul/a2_s2.sh
@@ -0,0 +1,23 @@
+#s2	Write a shell script which takes as parameters a text followed by several file names. The script will delete all the lines which contain the text given as parameter in all given files.
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Utilizare: $0 <text_cautat> <fisier1> <fisier2> ..."
+    exit 1
+fi
+
+TEXT_CAUTAT=$1
+
+shift
+
+for FISIER in "$@"; do
+
+    if [ -f "$FISIER" ]; then
+        echo "Se procesează fișierul: $FISIER"
+        sed -i "/$TEXT_CAUTAT/d" "$FISIER"
+    else
+        echo "Eroare: Fișierul $FISIER nu a fost găsit."
+    fi
+done
+
+echo "Operațiune finalizată."
