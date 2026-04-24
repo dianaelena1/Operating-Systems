383959e72b9c777c2a3efeeefa69042131b9cd8c
trxixn <sfarghiutraian999@gmail.com>
2026-04-24 10:46:56 +0300

    assignment4

---
+++ Homework/S2/Sfarghiu_Traian/assignment4/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+    echo "Connection Monitoring" 
+    
+    users=$(who | awk '{print $1}' | sort | uniq)
+    
+    for user in $users; do
+        proc_count=$(ps -u "$user" --no-headers | wc -l)
+        echo "User: $user | Active Processes: $proc_count"
+    done
+    
+    sleep 5
+done
