14bcc2e197aa38d947089fc45ecf0f85fe4775cf
Dragos <gugelaso@gmail.com>
2026-04-24 11:05:12 +0300

    deleted files a2 a3 a4

--- Homework/S2/Sarmasan Dragos/a2g.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in "$@"; do
-  if [[ -f "$file" ]]; then
-    
-    # 2nd and 4th words are set to empty, then we rebuilds the file without those words 
-    awk '{$2=$4=""}1'  "$file"
-  fi
-done
