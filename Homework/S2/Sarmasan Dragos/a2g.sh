6f24e730c757989ee61982ef140ae3c088089c9c
Dragos <gugelaso@gmail.com>
2026-04-24 11:02:33 +0300

    a2.a3.a4

---
+++ Homework/S2/Sarmasan Dragos/a2g.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for file in "$@"; do
+  if [[ -f "$file" ]]; then
+    
+    # 2nd and 4th words are set to empty, then we rebuilds the file without those words 
+    awk '{$2=$4=""}1'  "$file"
+  fi
+done
