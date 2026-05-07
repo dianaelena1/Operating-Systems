3f141942c318b0dc9831c76cbdebfb8ba76e8d29
Precop Alexandra-Maria <precopalexandramaria@gmail.com>
2026-05-08 00:35:32 +0300

    Assigment 6

---
+++ Homework/S1/Precop Alexandra Maria/Assignment 6 - pb 6/frequency_test.py
@@ -0,0 +1,31 @@
+import multiprocessing
+import string
+
+def count_letter(text, char, results_dict):
+    count = text.lower().count(char)
+    results_dict[char] = count
+
+def main():
+    print("Enter text: ")
+    sample_text = input(">")
+
+    manager = multiprocessing.Manager()
+    results = manager.dict()
+    processes = []
+    for letter in string.ascii_lowercase:
+        p = multiprocessing.Process(target=count_letter, args=(sample_text, letter, results))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print(f"{'Letter':<8} | {'Frequencu':<10}")
+    print("-" * 20)
+
+    for letter in sorted(results.keys()):
+        if results[letter] >0:
+            print(f"{letter.upper():<8} | {results[letter]:<10}")
+
+if __name__ == "__main__":
+    main()
